e7b3cc7632ecf31971a37a14f86d8166d62398a3
Tiến Hiệu <duongnguyen1216@gmail.com>
2017-10-20 10:37:05 +0700

    update hrm2017: init somepage

---
+++ hrm2017/hrm2017/DataMan.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace hrm2017
+{
+    //Data Manipulation
+    public class DataMan
+    {
+        public static string connectionString = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+        public static SqlConnection sqlConnection = new SqlConnection(connectionString);
+        public static SqlCommand sqlCommand;
+        public static SqlDataAdapter sqlDataAdapter;
+
+        public DataMan()
+        {
+            OpenConnection();
+        }
+
+        public static void CloseConnection()
+        {
+            if (sqlConnection.State != ConnectionState.Closed)
+                sqlConnection.Close();
+        }
+
+        public static void OpenConnection()
+        {
+            if (sqlConnection.State != ConnectionState.Open)
+                sqlConnection.Open();
+        }
+
+        public static DataTable GetDataTable(string sql)
+        {
+            OpenConnection();
+            sqlCommand = new SqlCommand(sql, sqlConnection);
+            sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            CloseConnection();
+            return dataTable;
+        }
+
+        public static void ExcuteCommand(string sql)
+        {
+            OpenConnection();
+            sqlCommand = new SqlCommand(sql, sqlConnection);
+            sqlCommand.ExecuteNonQuery();
+            CloseConnection();
+        }
+
+        public static int ExecuteCommandSingle(string sql)
+        {
+            OpenConnection();
+            sqlCommand = new SqlCommand(sql, sqlConnection);
+            int result = (int)sqlCommand.ExecuteScalar();
+            CloseConnection();
+            return result;
+        }
+    }
+}
---
+++ hrm2017/hrm2017/InterMan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace hrm2017
+{
+    //Interface Manipulation
+    public class InterMan
+    {
+        public static string ConvertToHtml(DataTable dataTable)
+        {
+            string table, tr, td, th = "";
+
+            for (int i = 0; i < dataTable.Columns.Count; i++)
+                th += "<th>" + dataTable.Columns[i].Caption + "</th>";
+            tr = "<tr class='w3-green'>" + th + "</tr>";
+
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                td = "";
+                for (int j = 0; j < dataTable.Columns.Count; j++)
+                    td += "<td>" + dataTable.Rows[i][j].ToString() + "</td>";
+                tr += "<tr>" + td + "</tr>";
+            }
+            table = "<table  class='w3-table w3-striped w3-bordered'>" + tr + "</table>";
+            return table;
+        }
+    }
+}
