beea1276afdb904b7d2c6090c5dfb91e294c4074
trannam96 <namit1951996@gmail.com>
2017-10-27 16:12:34 +0700

    add: Hồ sơ nhân viên :+1:

---
+++ hrm2017/hrm2017/forms/employee/HoSoNhanVien.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="HoSoNhanVien.aspx.cs" Inherits="hrm2017.forms.employee.HoSoNhanVien" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="content" runat="server">
+<div class="container">
+    <div class="content-box-large">
+        <div class="panel-heading">
+            <div class="panel-title">DANH SÁCH HỒ SƠ NHÂN VIÊN
+            </div>
+
+        </div>
+        <div class="panel-body">
+            <a href="#" class="btn btn-primary"><i class="glyphicon glyphicon-plus"></i> Thêm </a>
+            <br /><br />
+            <asp:Label ID="lbDsNhanVien" runat="server" />
+        </div>
+    </div>
+</div>
+</asp:Content>
---
+++ hrm2017/hrm2017/forms/employee/HoSoNhanVien.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace hrm2017.forms.employee
+{
+    public partial class HoSoNhanVien : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string sql = @"
+                SELECT ROW_NUMBER() over (order by manv) as 'STT'
+                ,[MANV] AS 'MÃ NHÂN VIÊN'
+                ,[HOTEN] AS 'HỌ TÊN'
+                ,CASE [GIOITINH] 
+				    WHEN 1 THEN 'Nam' 
+				    ELSE N'Nữ'
+			     END AS 'GIỚI TÍNH'
+                ,[NGAYSINH] AS 'NGÀY SINH'
+                ,[DIACHI] AS 'ĐỊA CHỈ'
+                ,[SODIENTHOAI] AS 'SỐ ĐIỆN THOẠI'
+                ,[TBL_CHUCVU].[TENCV] AS 'CHỨC VỤ'
+                ,[TBL_PHONGBAN].[TENPB] AS 'PHÒNG BAN'
+                FROM [HRM].[DBO].[TBL_NHANVIEN] 
+                LEFT JOIN [HRM].[DBO].[TBL_PHONGBAN] ON [HRM].[DBO].[TBL_NHANVIEN].[PHONGBAN] = [HRM].[DBO].[TBL_PHONGBAN].[MAPB]
+                LEFT JOIN TBL_CHUCVU ON [TBL_NHANVIEN].[CHUCVU] =  TBL_CHUCVU.MACV ";
+            DataTable dataTable = DataMan.GetDataTable(sql);
+            dataTable.Columns.Add("CHI TIẾT");
+
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+
+                int n = dataTable.Columns.Count-1;
+                string manv = dataTable.Rows[i][1].ToString();
+                dataTable.Rows[i][n] = string.Format("<a href='/ahihi?manv={0}'>Chi tiết</a>",manv);
+            }
+            lbDsNhanVien.Text= InterMan.ConvertToHtml(dataTable);
+        }
+    }
+}
