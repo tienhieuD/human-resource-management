13285e100462da3768f2366e7afa009e8181d029
Tiến Hiệu <duongnguyen1216@gmail.com>
2017-11-02 11:12:03 +0700

    UPDATE DATABASE

--- hrm2017/hrm2017/forms/salary/LapBangLuong.aspx.cs
+++ hrm2017/hrm2017/forms/salary/LapBangLuong.aspx.cs
@@ -11,12 +11,40 @@ namespace hrm2017.forms.salary
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Page.IsPostBack) return;
+            LoadListThangNam();
         }
+
+        private void LoadListThangNam()
+        {
+            if (lstNam.Items.Count > 0)
+            {
+                lstThang.SelectedValue = DateTime.Now.Month.ToString();
+                lstNam.SelectedValue = DateTime.Now.Year.ToString();
+                return;
+            }
+
+            for (int i = 1; i <= 12; i++)
+            {
+                string thang = i.ToString();
+                lstThang.Items.Add(new ListItem(thang, thang));
+            }
+
+            int year = DateTime.Now.Year;
+            for (int i = year - 1; i <= year + 1; i++)
+            {
+                string nam = i.ToString();
+                lstNam.Items.Add(new ListItem(nam, nam));
+            }
+
+            lstThang.SelectedValue = DateTime.Now.Month.ToString();
+            lstNam.SelectedValue = DateTime.Now.Year.ToString();
+        }
+
 
         protected void btnLapBangLuongThang_Click(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
